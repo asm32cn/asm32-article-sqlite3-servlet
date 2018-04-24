f915dac507c072a444dfc93ebacfb911f5ed4634
asm32 <asm32@qq.com>
2018-04-24 21:59:06 +0800

    asm32_article_web_servlet--linker.bat ... update

--- WEB-INF/source-code/asm32_article_web_servlet--linker.bat
+++ WEB-INF/source-code/asm32_article_web_servlet--linker.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set strCmd=javac -cp lib/servlet-api.jar -d ..\classes asm32_article_web_servlet.java
+set strCmd=javac -cp lib/servlet-api.jar -d ..\classes -encoding utf8 asm32_article_web_servlet.java
 echo #%strCmd%
 %strcmd%
 
